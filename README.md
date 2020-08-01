e22db4e5360a53bfcaa56f651416ae79fecdfabd
João Vitor da Costa Andrade <jvcostaandrade@edu.unifil.br>
2020-08-01 00:14:35 -0300

    icones

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# meet-chat-notifier
+# meet-chat-notifier 
+
+![](icon128.png)
+
 The extension was built to generate notification on your operating system of Google Meet chat messages, when the chat tab is open.
--- content.js
+++ content.js
@@ -1,20 +1,17 @@
 button = 0
-wait_buttons()
 
+wait_buttons()
 
 
 function wait_buttons(){
 
     setTimeout(function(){ 
 
-        let _button = document.querySelector('#ow3 > div.T4LgNb > div > div:nth-child(4) > div.crqnQb > div.pHsCke > div.Jrb8ue > div > div.NzPR9b > div:nth-child(3)')
+        button = document.querySelector('#ow3 > div.T4LgNb > div > div:nth-child(4) > div.crqnQb > div.pHsCke > div.Jrb8ue > div > div.NzPR9b > div:nth-child(3)')
 
-        if(_button){
+        if(button){
 
-            _button.click()
-
-            button = _button
-
+            button.click()
             
             chat_wait()
             
@@ -27,9 +24,8 @@ function wait_buttons(){
     }, 1000);
 }
 
-function chat_wait(){
 
-    
+function chat_wait(){   
 
     setTimeout(function(){ 
 
@@ -48,28 +44,6 @@ function chat_wait(){
 
 }
 
-function notifier_baloon(){
-    
-    var target = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(4) > div.crqnQb > div.NSvDmb.cM3h5d");
-    
-    function callback(mutationRecord, observer) {
-
-            showNotification('balloon',mutationRecord[0].addedNodes[0].parentNode)
-            
-    
-    }
-    
-    const observer = new MutationObserver(callback);
-    
-    const config = {
-      childList: true,
-      sutree: true
-    };
-    
-    observer.observe(target, config);
-
-}
-
 function notifier_btn(){
 
     button.addEventListener('click', ()=>{
@@ -83,10 +57,8 @@ function notifier_btn(){
     
     function callback(mutationRecord, observer) {
 
+            showNotification(mutationRecord[mutationRecord.length-1].addedNodes[0])
 
-            showNotification('chat',mutationRecord[mutationRecord.length-1].addedNodes[0])
-            
-    
     }
     
     const observer = new MutationObserver(callback);
@@ -100,45 +72,27 @@ function notifier_btn(){
 
 }
 
-function getNotificationId() {
-    var id = Math.floor(Math.random() * 9007199254740992) + 1;
-    return id.toString();
-  }
+function showNotification(message){
+ 
 
-function showNotification(type,message){
+            if(message.dataset.senderName){
+                senderName = message.dataset.senderName
+                message_text = message.lastChild.innerText
+            }else{
+                senderName = message.parentElement.parentNode.dataset.senderName
+                message_text = message.innerText
+            }
 
-    if(type == 'chat'){
-
-        if(message.dataset.senderName){
-            senderName = message.dataset.senderName
-            message_text = message.lastChild.innerText
-        }else{
-            senderName = message.parentElement.parentNode.dataset.senderName
-            message_text = message.innerText
-        }
-    }
-
-    if(type == 'balloon'){
-        
-        senderName = message.querySelectorAll("div > span > span > div > div.UgDTGe").innerText
-        message_text = message.querySelectorAll("div > span > span > div > div.mVuLZ").innerText
-    }
-
-
-        console.log(senderName,message_text)
-  
-        var opt = {
-            type: "basic",
-            title: senderName,
-            iconUrl: 'img_notification.png',
-            message: message_text,
-            silent: true,
-            priority: 2
-        };
-
-        chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
+            var opt = {
+                type: "basic",
+                title: senderName,
+                iconUrl: 'img_notification.png',
+                message: message_text,
+                silent: true,
+                priority: 2
+            };
     
-    
+            chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
 
 
 }
